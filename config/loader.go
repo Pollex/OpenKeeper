16c8dd879ffc95586821957ad9d2b9e6a86d2879
Tim van Osch <timvanosch@pollex.nl>
2024-05-06 15:59:52 +0200

    Feat: filepaths relative to config path by default

--- config/loader.go
+++ config/loader.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/knadh/koanf/parsers/toml/v2"
 	"github.com/knadh/koanf/providers/file"
@@ -28,5 +29,28 @@ func Load(location string) (Config, error) {
 		return config, fmt.Errorf("could not unmarshal configuration: %w", err)
 	}
 
-	return config, nil
+	return fixRelativePaths(config, location), nil
+}
+
+func fixRelativePaths(config Config, path string) Config {
+	absoluteConfigPath, _ := filepath.Abs(path)
+	absoluteWorkingDir := filepath.Dir(absoluteConfigPath)
+
+	for key := range config.OpenAPI3 {
+		subConfig := config.OpenAPI3[key]
+		if filepath.IsAbs(subConfig.File) {
+			continue
+		}
+		subConfig.File = filepath.Join(absoluteWorkingDir, subConfig.File)
+		config.OpenAPI3[key] = subConfig
+	}
+	for key := range config.TOML {
+		subConfig := config.TOML[key]
+		if filepath.IsAbs(subConfig.File) {
+			continue
+		}
+		subConfig.File = filepath.Join(absoluteWorkingDir, subConfig.File)
+		config.TOML[key] = subConfig
+	}
+	return config
 }
